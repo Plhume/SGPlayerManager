7eb580934504e6b12f6c528635303837ce2dfef3
Plhume <felix@exodius.fr>
2023-02-02 23:12:36 +0100

    Initial commit

---
+++ src/main/java/fr/plhume/sgpm/SGPlayersManager.java
@@ -0,0 +1,16 @@
+package fr.plhume.sgpm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SGPlayersManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // do stuff
+    }
+
+    @Override
+    public void onDisable() {
+        // do stuff
+    }
+}
